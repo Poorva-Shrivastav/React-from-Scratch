e5d30247b5f65afdcf0de7b8ae749a90b760aef1
Poorva-Shrivastav <35454765+Poorva-Shrivastav@users.noreply.github.com>
2023-12-31 19:00:55 +0530

    added carousel

--- src/components/Shimmer.js
+++ src/components/Shimmer.js
@@ -2,21 +2,20 @@ import React from "react";
 
 const Shimmer = () => {
   return (
-    <div className="w-11/12 mx-20 my-40 ">
-      <div className="w-80 h-4 mx-4 my-10 bg-gray-100 "></div>
+    <div className="w-full mx-4 my-10 ">
       <div className="flex flex-wrap">
-        <div className="p-4 m-4 w-[270px] h-40 bg-gray-100 rounded-2xl"></div>
-        <div className="p-4 m-4 w-[270px] h-40 bg-gray-100 rounded-2xl"></div>
-        <div className="p-4 m-4 w-[270px] h-40 bg-gray-100 rounded-2xl"></div>
-        <div className="p-4 m-4 w-[270px] h-40 bg-gray-100 rounded-2xl"></div>
-        <div className="p-4 m-4 w-[270px] h-40 bg-gray-100 rounded-2xl"></div>
-        <div className="p-4 m-4 w-[270px] h-40 bg-gray-100 rounded-2xl"></div>
-        <div className="p-4 m-4 w-[270px] h-40 bg-gray-100 rounded-2xl"></div>
-        <div className="p-4 m-4 w-[270px] h-40 bg-gray-100 rounded-2xl"></div>
-        <div className="p-4 m-4 w-[270px] h-40 bg-gray-100 rounded-2xl"></div>
-        <div className="p-4 m-4 w-[270px] h-40 bg-gray-100 rounded-2xl"></div>
-        <div className="p-4 m-4 w-[270px] h-40 bg-gray-100 rounded-2xl"></div>
-        <div className="p-4 m-4 w-[270px] h-40 bg-gray-100 rounded-2xl"></div>
+        <div className="p-4 m-4 w-[245px] h-40 bg-gray-100 rounded-2xl"></div>
+        <div className="p-4 m-4 w-[245px] h-40 bg-gray-100 rounded-2xl"></div>
+        <div className="p-4 m-4 w-[245px] h-40 bg-gray-100 rounded-2xl"></div>
+        <div className="p-4 m-4 w-[245px] h-40 bg-gray-100 rounded-2xl"></div>
+        <div className="p-4 m-4 w-[245px] h-40 bg-gray-100 rounded-2xl"></div>
+        <div className="p-4 m-4 w-[245px] h-40 bg-gray-100 rounded-2xl"></div>
+        <div className="p-4 m-4 w-[245px] h-40 bg-gray-100 rounded-2xl"></div>
+        <div className="p-4 m-4 w-[245px] h-40 bg-gray-100 rounded-2xl"></div>
+        <div className="p-4 m-4 w-[245px] h-40 bg-gray-100 rounded-2xl"></div>
+        <div className="p-4 m-4 w-[245px] h-40 bg-gray-100 rounded-2xl"></div>
+        <div className="p-4 m-4 w-[245px] h-40 bg-gray-100 rounded-2xl"></div>
+        <div className="p-4 m-4 w-[245px] h-40 bg-gray-100 rounded-2xl"></div>
       </div>
     </div>
   );
